4d52c596f3e6aab2387414b11bebbd9eac8f29c3
Sooraj Rajkumar <soorajrk007@gmail.com>
2023-05-02 22:36:24 +0530

    Add to cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
+import { Badge, Nav } from 'react-bootstrap';
+import { useContext } from 'react';
+import { Store } from './Store';
 
 function App() {
+  const {state} = useContext(Store);
+  const {cart} = state;
   return (
     <BrowserRouter>
       <div className='d-flex flex-column site-container'>
@@ -16,6 +21,15 @@ function App() {
               <LinkContainer to="/">
                 <Navbar.Brand>Amazon</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                    )}
+                    </Link></Nav>
             </Container>
           </Navbar>
 
