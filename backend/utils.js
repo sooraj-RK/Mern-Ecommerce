2c0b38012a56d26aab867b8c068075b34d3edf92
Sooraj Rajkumar <soorajrk007@gmail.com>
2023-05-11 15:28:16 +0530

    sign-in api

---
+++ backend/utils.js
@@ -0,0 +1,16 @@
+import Jwt from 'jsonwebtoken';
+
+export const generateToken = (user) => {
+  return Jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '30d',
+    }
+  );
+};
